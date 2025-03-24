02a88089ce63392ebf29282030d99de0cf0fc895
Lukuas31415 <LCBa@web.de>
2025-03-24 22:23:42 +0100

    Add first script for cluster

---
+++ opendihu_examples/Scrips_for_cluster/optimize_isotonic_force.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Job name:
+#SBATCH -J Optimize_isotonic_force
+#
+# Error and Output files
+#SBATCH -o ./%x.%j.out
+#SBATCH -e ./%x.%j.err
+#
+# Working directory:
+#SBATCH -D ./
+#
+#Notification and type
+#SBATCH --mail-type=END
+#SBATCH --mail-user=bauerls2@ipvs.uni-stuttgart.de
+#
+# Wall clock limit:
+#SBATCH --time=60:00:00
+#
+# Compute resources
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+
+echo "SLURM_NNODES"=$SLURM_NNODES
+echo "working directory="$SLURM_SUBMIT_DIR
+
+echo "Configuring enviroment variables"
+cd ../isotonic_contraction/optimize_prestretch_force/cuboid_muscle/build_release
+
+echo "Launching muscle"
+srun python BayesOpt_cuboid_muscle.py &> optimize_isotonic_force.log
+echo "Simulation completed."
