21c0a4fd219c2c0c355ed798efe700cb3d21385a
Lukuas31415 <LCBa@web.de>
2025-06-06 12:31:53 +0200

    Add files for dataset generation

---
+++ opendihu_examples/isometric_contraction/biceps_muscle/generate_datasets.py
@@ -0,0 +1,59 @@
+import numpy as np
+import shlex
+import subprocess
+import csv
+import os
+
+os.chdir("build_release")
+
+forces = np.linspace(0,10,21)
+
+for force in forces:
+
+    command = shlex.split(f"mpirun -n 16 ./muscle_contraction_with_prestretch ../settings_contraction_with_prestretch.py ramp.py --prestretch_force {force} --scenario_name {force}")
+    subprocess.run(command)
+
+    tractionz_rank0 = []
+    tractionz_rank1 = []
+    tractionz_rank2 = []
+    tractionz_rank3 = []
+
+    f = open("out/prestretch" + str(force) + "/muscle_contraction_rank0.csv")
+    reader = csv.reader(f)
+    for row in reader:
+        tractionz_rank0.extend([float(value) if value.strip() else 0 for value in row])
+    f.close()
+
+    f = open("out/prestretch" + str(force) + "/muscle_contraction_rank1.csv")
+    reader = csv.reader(f)
+    for row in reader:
+        tractionz_rank1.extend([float(value) if value.strip() else 0 for value in row])
+    f.close()
+
+    f = open("out/prestretch" + str(force) + "/muscle_contraction_rank2.csv")
+    reader = csv.reader(f)
+    for row in reader:
+        tractionz_rank2.extend([float(value) if value.strip() else 0 for value in row])
+    f.close()
+
+    f = open("out/prestretch" + str(force) + "/muscle_contraction_rank3.csv")
+    reader = csv.reader(f)
+    for row in reader:
+        tractionz_rank3.extend([float(value) if value.strip() else 0 for value in row])
+    f.close()
+
+    f = open("out/prestretch" + str(force) + "/muscle_prestretch_rank0.csv")
+    reader = csv.reader(f)
+    for row in reader:
+        length_after_prestretch = row[1]
+    f.close()
+
+    # we look at the max in absolute value, and we assume it will be negative
+    maxtraction = max((np.array(tractionz_rank0)+np.array(tractionz_rank1)+np.array(tractionz_rank2)+np.array(tractionz_rank3))/4)
+
+    print("The maximum traction was ", maxtraction)
+
+    f = open("biceps_dataset.csv", "a")
+    f.write(str(force) + "," + str(maxtraction))
+    f.write("\n")
+    f.close()
---
+++ opendihu_examples/isometric_contraction/biceps_muscle/generate_datasets.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Job name:
+#SBATCH -J Generate_datasets
+#
+# Error and Output files
+#SBATCH -o ./%x.%j.out
+#SBATCH -e ./%x.%j.err
+#
+# Working directory:
+#SBATCH -D ./
+#
+#Notification and type
+#SBATCH --mail-type=END
+#SBATCH --mail-user=bauerls2@ipvs.uni-stuttgart.de
+#
+# Wall clock limit:
+#SBATCH --time=60:00:00
+#
+# Compute resources
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+
+echo "SLURM_NNODES"=$SLURM_NNODES
+echo "working directory="$SLURM_SUBMIT_DIR
+
+echo "Configuring enviroment variables"
+
+echo "Launching muscle"
+srun python generate_datasets.py &> Generate_datasets.log
+echo "Simulation completed."
